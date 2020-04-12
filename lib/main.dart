73e80eac27af14f3a2842ee10c3006bfed47e55c
MatGhp <mat.ghp@gmail.com>
2020-04-12 16:17:03 +0200

    add more icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-            backgroundColor: Colors.deepOrangeAccent,
-            appBar: AppBar(
-              title: Text('Poor Title (in progress...)'),
-              backgroundColor: Colors.redAccent,
-            ),
-            body: Center(
-              child: Image(
-                image: AssetImage(
-                  "images/me.jpg",
-                ),
-              ),
-            )),
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(backgroundColor: Colors.red, title: Text('Some Title')),
+        body: Center(
+          child: Text('My name is Mojtaba'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.redAccent,
+          child: Icon(Icons.add),
+        ),
       ),
     );
+  }
+}
