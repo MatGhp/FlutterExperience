d724848c6c8457fcf65865bf8b4cba91cc3242e8
MatGhp <mat.ghp@gmail.com>
2020-04-27 23:07:37 +0200

    add more rows to the main component

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,41 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(backgroundColor: Colors.red, title: Text('Some Title')),
-        body: Center(
-          child: Text('My name is Mojtaba'),
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                  height: 100,
+                  color: Colors.deepOrangeAccent,
+                  child: Row(children: <Widget>[
+                    Icon(Icons.star, size: 50),
+                    Icon(Icons.star, size: 50),
+                    Icon(Icons.star, size: 50)
+                  ])),
+              SizedBox(height: 3),
+              Container(
+                  height: 100,
+                  color: Colors.redAccent,
+                  child: Row(children: <Widget>[
+                    Icon(Icons.adjust, size: 50),
+                    Icon(Icons.adjust, size: 50),
+                    Icon(Icons.adjust, size: 50)
+                  ])),
+              SizedBox(height: 3),
+              Container(
+                  height: 100,
+                  color: Colors.deepOrange,
+                  child: Row(children: <Widget>[
+                    Icon(Icons.access_time, size: 50),
+                    Icon(Icons.access_time, size: 50),
+                    Icon(Icons.access_time, size: 50),
+                  ])),
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.redAccent,
+          backgroundColor: Colors.red,
           child: Icon(Icons.add),
         ),
       ),
